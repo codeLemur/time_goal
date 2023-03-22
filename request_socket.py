ff244f7350bfa8fb7b1603f7451fe2d8cf8ab83c
codeLemur <lars.kipfer55@gmail.com>
2023-03-22 21:24:47 +0100

    New: Request socket for goal

---
+++ request_socket.py
@@ -0,0 +1,77 @@
+import requests
+import csv
+import globals
+import logging
+import os
+from datetime import datetime
+
+
+class RequestSocket:
+    URL = 'http://192.168.1.82'
+    DATA_PATH = 'data'
+    GOAL_TIME_FILENAME = os.path.join(DATA_PATH, 'goal_time.csv')
+
+    def __init__(self, role: str):
+        self._role = role
+        self._current_shadow_state = globals.States.IDLE
+        self.goal_time_fieldnames = [globals.TIMESTAMP_KEY, globals.LOG_TIME_KEY]
+
+        if not os.path.exists(self.DATA_PATH):
+            os.makedirs(self.DATA_PATH)
+        if not os.path.exists(self.GOAL_TIME_FILENAME):
+            with open(self.GOAL_TIME_FILENAME, 'w', newline='') as goal_time_file:
+                writer = csv.DictWriter(goal_time_file, fieldnames=self.goal_time_fieldnames)
+                writer.writeheader()
+
+    def post_timestamp(self, timestamp_ms: int):
+        logging.info(timestamp_ms)
+        data = {
+            globals.ROLE_KEY: self._role,
+            globals.COMMAND_TYPE_KEY: globals.CMD_REPORT_TIME,
+            globals.TIMESTAMP_KEY: timestamp_ms,
+        }
+        with open(self.GOAL_TIME_FILENAME, 'a', newline='') as goal_time_file:
+            writer = csv.DictWriter(goal_time_file, fieldnames=self.goal_time_fieldnames)
+            writer.writerow({globals.TIMESTAMP_KEY: timestamp_ms,
+                             globals.LOG_TIME_KEY: datetime.fromtimestamp(time.time())})
+        self._post(data)
+
+    def send_event(self, event: globals.Events):
+        logging.info(f'Sending event: {event.name}')
+        data = {
+            globals.ROLE_KEY: self._role,
+            globals.COMMAND_TYPE_KEY: globals.CMD_STATE_CHANGE,
+            globals.EVENT_KEY: event.name
+        }
+        self._post(data)
+
+    def request_current_state(self):
+        try:
+            response = requests.get(self.URL, timeout=2)
+            logging.info(f'Current state: {response.text}')
+            self._current_shadow_state = globals.States[response.text]
+        except requests.exceptions.ConnectTimeout:
+            logging.error(f"Connection timeout could not reach: {self.URL}")
+            # TODO change state to ERROR?
+        except KeyError:
+            logging.error(f"KeyError: Received state is invalid")
+
+    def get_current_state(self):
+        return self._current_shadow_state
+
+    def _post(self, data: dict):
+        try:
+            response = requests.post(self.URL, json=data, timeout=2)
+            if response.status_code == 200:
+                logging.info(f'Server response: {response.text}')
+            elif response.status_code == 400:
+                logging.error("Request failed")
+            else:
+                logging.warning(f'status_code {response.status_code}')
+        except requests.exceptions.ConnectTimeout:
+            logging.error(f"Connection timeout could not reach: {self.URL}")
+
+
+if __name__ == '__main__':
+    import time
+    logging.basicConfig(level=logging.DEBUG)
