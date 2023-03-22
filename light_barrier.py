b11fc022a136e872bf5f4d76a0c01a3733d23f4a
codeLemur <lars.kipfer55@gmail.com>
2023-03-22 21:22:47 +0100

    Mod: Mock lightbarrier on windows

--- light_barrier.py
+++ light_barrier.py
@@ -1,12 +1,19 @@
-import RPi.GPIO as GPIO
+import os
+
+if os.name != 'nt':
+    import RPi.GPIO as GPIO
 
 
 class LightBarrier:
     INPUT_PIN = 17  # Broadcom pin 17 (P1 pin 11)
 
     def __init__(self, is_active_high: bool = True) -> None:
-        GPIO.setmode(GPIO.BCM)  # Broadcom pin-numbering scheme
-        GPIO.setup(self.INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TODO PUP required?
+        if os.name != 'nt':
+            GPIO.setmode(GPIO.BCM)  # Broadcom pin-numbering scheme
+            GPIO.setup(self.INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def is_activated(self) -> bool:
-        return GPIO.input(self.INPUT_PIN)
+        if os.name == 'nt':
+            return True
+        else:
+            return GPIO.input(self.INPUT_PIN)
