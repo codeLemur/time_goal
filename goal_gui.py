0d1f8aca23cbaee865e114e7b46581cd6d7d607f
codeLemur <lars.kipfer55@gmail.com>
2023-03-23 20:40:45 +0100

    Mod: Display calculated race time

--- goal_gui.py
+++ goal_gui.py
@@ -3,13 +3,14 @@ import logging
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty, NumericProperty, ListProperty
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty
 from kivy.core.window import Window
 from kivy.graphics import Color, Rectangle
 from kivy.clock import Clock
 from kivy.clock import mainthread
 
 import time
+from datetime import datetime
 from threading import Thread
 
 import globals
@@ -17,7 +18,7 @@ from light_barrier import LightBarrier
 from request_socket import RequestSocket
 
 NS_PER_MS = int(1e6)
-STATUS_POLL_PERIOD_S = 2.5
+STATUS_POLL_PERIOD_S = 0.1
 
 RED = (1, 0, 0)
 GREEN = (0, 0.4, 0)
@@ -35,16 +36,27 @@ COLOR_LOOKUP = {
 
 class GoalScreen(Screen):
     start_number = NumericProperty(0)
-    current_time = NumericProperty(0)
-    duration_time = NumericProperty(0)
+    current_time = StringProperty("00:00")
+    duration_time = StringProperty("00:00")
 
     def set_ready(self):
         logging.info('set_ready')
         request_socket.send_event(globals.Events.SET_READY)
+        self.set_duration_time('00:00')
+        self.set_current_time('00:00')
 
     def confirm_time(self):
         logging.info('confirm_time')
         request_socket.send_event(globals.Events.STOP)
+        self.current_time = self.duration_time
+
+    def set_current_time(self, current_time):
+        logging.info(f'Changed current time to: {current_time}')
+        self.current_time = current_time
+
+    def set_duration_time(self, duration_time):
+        logging.info(f'Changed duration time to: {duration_time}')
+        self.duration_time = duration_time
 
     @mainthread
     def set_system_state(self, state: str, color: tuple):
@@ -75,6 +87,7 @@ sm.current = "goal"
 
 class GoalApp(App):
     system_status = globals.States.IDLE
+    start_time = 0
 
     def build(self):
         Thread(target=observe_lightbarrier).start()
@@ -88,26 +101,33 @@ class GoalApp(App):
     def poll_system_status(self):
         request_socket.request_current_state()
         logging.info(f'Time2: {time.time()}')
+        previous_state = self.system_status
         self.system_status = request_socket.get_current_state()
         logging.info(f'Current status: {self.system_status}')
         goal_screen = sm.get_screen('goal')
         goal_screen.set_system_state(self.system_status.name, COLOR_LOOKUP[self.system_status])
+        if self.system_status == globals.States.RUNNING and previous_state == globals.States.READY:
+            self.start_time = time.time()
         if self.system_status == globals.States.RUNNING:
             goal_screen.confirm_button.disabled = False
             goal_screen.ready_button.disabled = True
+            # Update current time
+            goal_screen.set_current_time(f'{datetime.fromtimestamp(time.time() - app.start_time).strftime("%M:%S.%f")[:-5]}')
         elif self.system_status in [globals.States.IDLE, globals.States.STOPPED]:
             goal_screen.ready_button.disabled = False
             goal_screen.confirm_button.disabled = True
         else:
             goal_screen.confirm_button.disabled = True
-            goal_screen.ready_button.disabled = True
+
 
 def observe_lightbarrier():
     while True:
         if light_barrier.is_activated():
             if app.system_status == globals.States.RUNNING:
-                request_socket.post_timestamp(int(time.time_ns() / NS_PER_MS))
-                time.sleep(1) # TODO remove me
+                timestamp_ms = int(time.time_ns() / NS_PER_MS)
+                request_socket.post_timestamp(timestamp_ms)
+                sm.get_screen('goal').set_duration_time(f'{datetime.fromtimestamp(time.time() - app.start_time).strftime("%M:%S.%f")[:-5]}')
+                time.sleep(1)  # TODO remove me
         else:
             pass
             # logging.info("Light barrier Deactivated")
