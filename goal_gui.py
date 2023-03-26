3009a05d6317bcc4eac7da5ff2c763a8a2524edc
codeLemur <lars.kipfer55@gmail.com>
2023-03-26 18:37:39 +0200

    Mod: Static windows size

--- goal_gui.py
+++ goal_gui.py
@@ -1,7 +1,7 @@
 import logging
 
 from kivy.core.window import Window
-Window.fullscreen = True
+Window.size = (1024, 600)
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
