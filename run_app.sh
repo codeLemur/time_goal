ee615ab968ddc2ddeb6607bf65ba31742a261433
PC-1\larsk <lars.kipfer55@gmail.com>
2024-03-17 17:17:31 +0100

    Fix: venv activation

---
+++ run_app.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+
+# Absolute path to the directory containing your virtual environment
+VENV_DIR = "/home/pi/repo/time_goal/venv/"
+
+# Absolute path to your Python script
+PYTHON_SCRIPT = "/home/pi/repo/time_goal/goal_gui.py"
+
+# Activate the virtual environment
+source "$VENV_DIR/bin/activate"
+
+# Run the Python script
+python3 "$PYTHON_SCRIPT"
+
+# Deactivate the virtual environment
+deactivate
