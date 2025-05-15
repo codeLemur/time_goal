5d5f5bfa58e86f0540fa4562c02feaa48b9c51dc
PC-1\larsk <lars.kipfer55@gmail.com>
2025-05-15 20:23:30 +0200

    Changes from race day 2024

--- light_barrier.py
+++ light_barrier.py
@@ -3,6 +3,9 @@ import time
 
 if os.name != 'nt':
     import RPi.GPIO as GPIO
+else:
+    import board
+    import digitalio
 
 
 class LightBarrier:
@@ -10,12 +13,16 @@ class LightBarrier:
 
     def __init__(self, is_active_high: bool = True) -> None:
         self.current_state = False
+        self.button = None
         if os.name != 'nt':
             GPIO.setmode(GPIO.BCM)  # Broadcom pin-numbering scheme
             GPIO.setup(self.INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        else:
+            self.button = digitalio.DigitalInOut(board.C0)
+            self.button.direction = digitalio.Direction.INPUT
 
     def is_activated(self) -> bool:
         if os.name == 'nt':
-            return (int(time.time()) % 2) == 0
+            return not self.button.value
         else:
             return GPIO.input(self.INPUT_PIN)
